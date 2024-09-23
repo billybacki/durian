f6d758f5620a66f14ea96731def26e4e4e5b2e10
dev <>
2024-09-23 23:54:41 +0800

    feat: add vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import svgr from 'vite-plugin-svgr'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react(), svgr(), nodePolyfills()],
+  plugins: [react(), svgr(), nodePolyfills(), VitePWA()],
   optimizeDeps: {
     include: ['@emotion/styled']
   },
